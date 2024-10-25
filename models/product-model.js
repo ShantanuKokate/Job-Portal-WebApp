318b6683817b26496ad19ef2f39cefe4c1256eba
Shantanu Kokate <you@example.com>
2024-10-25 10:54:03 +0530

    models

---
+++ models/product-model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    Exp: {
+      type: Number,
+      required: true,
+    },
+    internship: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("product", productSchema);
